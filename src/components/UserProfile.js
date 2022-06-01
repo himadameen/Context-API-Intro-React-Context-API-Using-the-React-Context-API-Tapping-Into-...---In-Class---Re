bf3cb2f2d87040cf1b9b5982dda7f3745356e6f0
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-06-01 13:04:37 +0530

    my changes

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,15 @@
-import { useContext } from 'react'
-import React from 'react'
-import { UserContext } from "./App"
+import { useContext } from "react";
+import React from "react";
+import { UserContext } from "./App";
 
-const UserProfile = () =>{
+const UserProfile = () => {
+  const text = useContext(UserContext);
+  return (
+    <>
+      <h1 id="name">Name:- {text.name}</h1>
+      <h1 id="age">Age:- {text.age}</h1>
+    </>
+  );
+};
 
-}
-
-export {UserProfile}
+export { UserProfile };
